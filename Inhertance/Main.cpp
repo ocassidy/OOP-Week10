30cc1c16e3544b478e9dedf3b40b1e05810dc189
ocassidy <oisin.c@hotmail.co.uk>
2017-11-28 14:29:40 +0000

    videos not finished

--- Inhertance/Main.cpp
+++ Inhertance/Main.cpp
@@ -13,46 +13,65 @@
 #include <string>
 #include <vector>
 #include "Book.h"
+#include <fstream>
 #include "Publisher.h"
 #include "Author.h"
 #include "PhysicalBook.h"
 #include "EBook.h"
 
-using namespace std;
-
 int main() {
 
-	Author bjarne{ "Bjarne", "Stroustrup" };
-	Book myBook{"Programming principles and practice using C++", bjarne, "Addison Wesley", "2343242", bookGenre::Reference, 59.99f};
+
+
+	std::ofstream outFile{};
+
+	outFile.open("data.txt", std::ios::out | std::ios::app);
+
+	if (!outFile) {
+		std::cerr << "File could not be opened" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	outFile << "Henloo" << std::endl;
+	outFile << "U stpid birbb" << std::endl;
+	outFile << "eat a seedd" << std::endl;
 	
-	//cout << myBook.ToString();
+	outFile.close();
 
-	Author aaron{ "Aaron ", "Franklin" };
-	PhysicalBook myPB{ "Franklin Barbecue (A Meatsmoking Manifesto)", aaron, "Ten Speed Press", "978-1607747208", bookGenre::Cuisine, 17.46f, 0.8f, 21.0f, 26.1f, 2.6f };
 
-	EBook myEB{ "Franklin Barbecue (A Meatsmoking Manifesto)", aaron, "Ten Speed Press", "978-1607747208", bookGenre::Cuisine, 17.46f, "Kindle Edition", 63145.0f, "English", true, true  };
 
-	myPB.SetTitle("hello sailor");
 
-	//cout << myPB.ToString();
-	//cout << myEB.ToString();
+	//Author bjarne{ "Bjarne", "Stroustrup" };
+	//Book myBook{"Programming principles and practice using C++", bjarne, "Addison Wesley", "2343242", bookGenre::Reference, 59.99f};
+	//
+	////cout << myBook.ToString();
 
-	Book *bookPtr{ nullptr };
-	bookPtr = &myBook;
-	cout << "Calling toString using base book pointer on a book object" << endl << endl;
-	cout << bookPtr->ToString();
+	//Author aaron{ "Aaron ", "Franklin" };
+	//PhysicalBook myPB{ "Franklin Barbecue (A Meatsmoking Manifesto)", aaron, "Ten Speed Press", "978-1607747208", bookGenre::Cuisine, 17.46f, 0.8f, 21.0f, 26.1f, 2.6f };
 
-	PhysicalBook *pBookPtr{ nullptr };
-	pBookPtr = &myPB;
-	cout << "Calling toString using base book pointer on a Physical Book object" << endl << endl;
-	cout << pBookPtr->ToString();
+	//EBook myEB{ "Franklin Barbecue (A Meatsmoking Manifesto)", aaron, "Ten Speed Press", "978-1607747208", bookGenre::Cuisine, 17.46f, "Kindle Edition", 63145.0f, "English", true, true  };
 
-	bookPtr = &myPB;
-	cout << "Calling toString using base book pointer on a derived physical book object" << endl << endl;
-	cout << bookPtr->ToString();
+	//myPB.SetTitle("hello sailor");
 
-	bookPtr = nullptr;
-	pBookPtr = nullptr;
+	////cout << myPB.ToString();
+	////cout << myEB.ToString();
+
+	//Book *bookPtr{ nullptr };
+	//bookPtr = &myBook;
+	//cout << "Calling toString using base book pointer on a book object" << endl << endl;
+	//cout << bookPtr->ToString();
+
+	//PhysicalBook *pBookPtr{ nullptr };
+	//pBookPtr = &myPB;
+	//cout << "Calling toString using base book pointer on a Physical Book object" << endl << endl;
+	//cout << pBookPtr->ToString();
+
+	//bookPtr = &myPB;
+	//cout << "Calling toString using base book pointer on a derived physical book object" << endl << endl;
+	//cout << bookPtr->ToString();
+
+	//bookPtr = nullptr;
+	//pBookPtr = nullptr;
 
 	return 0;
 }
